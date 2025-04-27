64e433fa09ec17dc1b229f3767968052f6f7ebdf
ak-ash93 <ash9793ak@gmail.com>
2025-04-28 02:35:11 +0800

    Project completed

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsers } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsers,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsers);
 router.get("/:id", protectRoute, getMessages);
-router.post("/send/:id", protectRoute);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
